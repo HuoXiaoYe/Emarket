d8c17c3453c8fada6597eaabaa06a09a291495e3
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-01 22:59:57 +0800

    页面构建1

---
+++ lib/pages/indexPage.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("霍小叶电商项目"),
+      ),
+      body: Center(
+        child: Text("霍小叶电商项目"),
+      ),
+    );
+  }
+}
