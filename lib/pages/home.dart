ab923d4a19b8763faffbe595b470a9852d636ba1
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-02 16:05:31 +0800

    banner

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,121 +1,56 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 import '../service/service_methods.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
-  @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String homePageContent = "正在获取数据";
-  @override
-  void initState() {
-    // 获取远端的数据
-    getHomePageContent().then((val){
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
-    super.initState();
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("霍小叶电商项目"),),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
+        appBar: AppBar(
+          title: Text('霍小叶电商项目'),
+        ),
+        body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context, snapshot) {
+            List swiperDataList = [
+              "images/banner/a1.jpg",
+              "images/banner/a2.jpg",
+              "images/banner/a3.jpg",
+              "images/banner/a4.jpg"
+            ].cast();
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiperDataList), //页面顶部轮播组件
+              ],
+            );
+          },
+        ));
+  }
+}
+
+// 首页轮播组件编写
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.asset("${swiperDataList[index]}",
+              fit: BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: new SwiperPagination(),
+        autoplay: true,
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import "package:flutter/material.dart";
-
-// // 引入dio库
-// import "package:dio/dio.dart";
-
-
-
-
-
-// class HomePage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     getHttp();
-//     return Scaffold(
-//       body: Center(child: Text("商城首页"),),
-//     );
-//   }
-
-
-
-
-//   void getHttp() async{
-//     try{
-//       Response response;
-//       const url = "https://www.easy-mock.com/mock/5c60131a4bed3a6342711498/baixing/dabaojian?name=长腿小姐姐";
-//       response = await Dio().get(url);
-//       return print(response);
-//     }catch(e){
-//       print(e);
-//     }
-//   }
-// }
