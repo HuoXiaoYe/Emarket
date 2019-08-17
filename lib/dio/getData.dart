e2e65a3c800b24fe60c674135ff75578f98f70e9
huoxiaoye <17711034@bjtu.edu.cn>
2019-08-17 09:14:18 +0800

    商品数据请求成功

--- lib/dio/getData.dart
+++ lib/dio/getData.dart
@@ -1,19 +1,5 @@
 import 'package:dio/dio.dart';
 import 'dart:convert';
-// 获取分类页面分类的函数
-// void getCategory() async {
-//   print(servicePath['category']);
-//   try {
-//     Response response = await Dio().get(servicePath['category']);
-//     var data = json.decode(response.toString());
-//     CategoryModel list = CategoryModel.fromJson(data);
-//      list.data.forEach((item)=>print(item.mallCategoryName));
-//   } catch (e) {
-//     print(e);
-//   }
-// }
-
-
 // 异步请求数据的方法
 Future request(url) async {
     Response response = await Dio().get(url);
@@ -26,7 +12,6 @@ Future request(url) async {
 // 函数 测试
 // getData() async{
 //   await request(servicePath['category']).then((val){
-//     // print()
 //     CategoryModel list = CategoryModel.fromJson(val);
 //     print(list.data);
 //     list.data.map((item){
--- lib/pages/goodsDetail.dart
+++ lib/pages/goodsDetail.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../dio/getData.dart';
+import '../config/service_url.dart';
+import '../model/GoodsModel.dart';
+import 'dart:convert';
 
 
 class GoodsDetail extends StatefulWidget {
@@ -7,10 +11,27 @@ class GoodsDetail extends StatefulWidget {
 }
 
 class _GoodsDetailState extends State<GoodsDetail> {
+  var goodsInfo;
   @override
   Widget build(BuildContext context) {
+    _getGoodsInfo();
     return Container(
-      child:Text("商品详情")
+      child:Text("goodsInfo")
     );
+  }
+
+
+  _getGoodsInfo() async{
+    await request(servicePath['goodsdetail']).then((val){
+      // print(val['data']['goodInfo']['amount']);
+      GoodsDetailModel info = GoodsDetailModel.fromJson(val);
+      
+      
+      // print(info.data.goodInfo.isOnline);
+      setState(() {
+        goodsInfo = info.data;
+        print(info.data.goodInfo.isOnline);
+      });
+    });
   }
 }
